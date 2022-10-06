c0e262616466f1d402e2e3110bdfa242648b8ad3
sneurlax <sneurlax@gmail.com>
2022-10-05 22:16:47 -0500

    Add template tests file
    Doesn't do anything yet, need to isolate node connection code first then wallet creation code next (where's the network type bit set?)

---
+++ test/services/coins/monero/monero_wallet_test.dart
@@ -0,0 +1,14 @@
+import 'package:decimal/decimal.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:hive/hive.dart';
+import 'package:hive_test/hive_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+import 'package:stackwallet/services/coins/monero/monero_wallet.dart';
+import 'package:stackwallet/utilities/flutter_secure_storage_interface.dart';
+import 'package:tuple/tuple.dart';
+
+@GenerateMocks(
+    [])
+void main() {
+}
