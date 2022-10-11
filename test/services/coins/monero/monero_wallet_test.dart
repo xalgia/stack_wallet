2469732af8564664e77f66e6dfcd22067f848f29
sneurlax <sneurlax@gmail.com>
2022-10-10 19:52:08 -0500

    Instantiate a stagenet MoneroWallet

--- test/services/coins/monero/monero_wallet_test.dart
+++ test/services/coins/monero/monero_wallet_test.dart
@@ -5,10 +5,32 @@ import 'package:hive_test/hive_test.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
 import 'package:stackwallet/services/coins/monero/monero_wallet.dart';
+import 'package:stackwallet/services/price.dart';
+import 'package:stackwallet/services/transaction_notification_tracker.dart';
+import 'package:stackwallet/utilities/enums/coin_enum.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter_libmonero/core/key_service.dart';
+import 'package:flutter_libmonero/core/wallet_creation_service.dart';
+import 'package:flutter_libmonero/monero/monero.dart';
+import 'package:flutter_libmonero/view_model/send/output.dart' as monero_output;
 import 'package:stackwallet/utilities/flutter_secure_storage_interface.dart';
 import 'package:tuple/tuple.dart';
 
-@GenerateMocks(
-    [])
+@GenerateMocks([PriceAPI, TransactionNotificationTracker])
 void main() {
+  group("1", () {
+    FakeSecureStorage? secureStore;
+
+    MoneroWallet? stagenetWallet;
+    setUp(() {
+      secureStore = FakeSecureStorage();
+
+      stagenetWallet = MoneroWallet(
+        walletId: "validateAddressStageNet",
+        walletName: "validateAddressStageNet",
+        coin: Coin.moneroStageNet,
+        // tracker: tracker,
+      );
+    });
+  });
 }
